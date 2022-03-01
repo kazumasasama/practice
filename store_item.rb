835b0ef83c090fba9b5345b54b3b47b9c0608e38
Kaz <takaik@yahoo.com>
2022-03-01 14:59:28 -0500

    first commit

---
+++ store_item.rb
@@ -0,0 +1,65 @@
+# C. Use hashes with symbols to represent the following scenario:
+
+# C.1 - You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+
+# apple = {
+#   :color => "Red",
+#   :price => 1.00,
+#   :brand => "Fuji",
+# }
+
+# banana = {
+#   :color => "Yellow",
+#   :price => 5.00,
+#   :brand => "Banannnnaa",
+# }
+
+# pinapple = {
+#   :color => "",
+#   :price => 6.00,
+#   :brand => "whatever",
+# }
+
+
+apple = {
+  color: "Red",
+  price: 1.00,
+  brand: "Fuji",
+}
+
+banana = {
+  color: "Yellow",
+  price: 5.00,
+  brand: "Banannnnaa",
+}
+
+pinapple = {
+  color: "Yellow",
+  price: 6.00,
+  brand: "whatever",
+}
+
+
+p apple
+p banana
+p pinapple
+
+
+# C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
+
+
+
+# C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
+# Bonus: Read more about Ruby array and hash methods. Explore using different methods in your code.
+# https://ruby-doc.org/core/Array.html
+# https://ruby-doc.org/core/Hash.html 
+# https://ruby-doc.org/core/Symbol.html 
+# ruby-doc.orgruby-doc.org
+# Class: Array (Ruby 3.1.1)
+# Class : Array - Ruby 3.1.1
+# ruby-doc.orgruby-doc.org
+# Class: Hash (Ruby 3.1.1)
+# Class : Hash - Ruby 3.1.1
+# ruby-doc.orgruby-doc.org
+# Class: Symbol (Ruby 3.1.1)
+# Class : Symbol - Ruby 3.1.1
