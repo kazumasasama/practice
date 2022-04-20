442de45b0edacb4d7b738a8526482206c115136d
Kaz <takaik@yahoo.com>
2022-04-19 20:15:17 -0400

    add today's practice

---
+++ ruby/methods_practice_problems.rb
@@ -0,0 +1,82 @@
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+# print "Enter a word: "
+# word = gets.chomp
+# p word.upcase
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+# print "Enter a number: "
+# num = gets.chomp.to_i
+# if num > 100
+#   p "That's a big number"
+# end
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+# puts "Enter two numbers one at a time."
+# nums = []
+# print "Number 1: "
+# nums << gets.chomp.to_i
+# print "Number 2: "
+# nums << gets.chomp.to_i
+# p nums.sum
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+# print "Enter a word: "
+# word = gets.chomp
+# p word.reverse
+
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+# print "Enter a word: "
+# word = gets.chomp
+# p word * 10
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+# puts "Enter two words one at a time."
+# words = []
+# print "Word 1: "
+# words << gets.chomp
+# print "Word 2: "
+# words << gets.chomp
+# p "#{words[0]} #{words[1]}".upcase
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# puts "Enter a word: "
+# word = gets.chomp
+# p word.length
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+# puts "Enter a number"
+# num = gets.chomp.to_i
+# if num < 0
+#   p "That's a negative number"
+# end
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+# puts "Enter two numbers one at a time."
+# nums = []
+# print "Number 1: "
+# nums << gets.chomp.to_i
+# print "Number 2: "
+# nums << gets.chomp.to_i
+# p nums[0] * nums[1]
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+# print "Enter a word"
+# word = gets.chomp
+# if word.length > 5
+#   p "That's a long word"
+# end
+
+
+# 1: 15 mins
+
+# SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
---
+++ ruby/strings_practice_problems.rb
@@ -0,0 +1,75 @@
+# # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+# first_name = "Nobi"
+# last_name = "Nobita"
+# puts first_name + " " + last_name
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+# first_name = "Big"
+# last_name = "G"
+# puts "#{first_name} #{last_name}"
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+# print "Enter a word: "
+# word = gets.chomp
+# if word == "marco"
+#   p "polo"
+# end
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+# color_1 = "red"
+# color_2 = "blue"
+# color_3 = "black"
+# puts color_1 + " " + color_2 + " " + color_3
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+# color_1 = "red"
+# color_2 = "blue"
+# color_3 = "black"
+# puts "#{color_1}, #{color_2}, #{color_3}"
+
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+# print "Enter a name: "
+# name = gets.chomp
+# if name != "Santa"
+#   p "You're not Santa."
+# end
+
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+# title = "The World"
+# author = "No Name"
+# puts title + ": Written by " + author
+
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+# title = "The World"
+# author = "No Name"
+# puts "#{title}: was written by #{author}"
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+# print "Enter your password: "
+# password = gets.chomp
+# if password == "Joshua"
+#   p "Shall we play a game?"
+# else
+#   p "Access denied"
+# end
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+# city_1 = "New York"
+# city_2 = "Chicago"
+# city_3 = "San Fransisco"
+# puts city_1 + city_2 + city_3
+
+
+# 1: 18 mins
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
