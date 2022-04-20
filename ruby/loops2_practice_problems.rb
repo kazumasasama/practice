d121873270b8727b2e7c2d127fffd4f167b0d948
Kaz <takaik@yahoo.com>
2022-04-19 20:31:41 -0400

    add solution for loops2_practice_problems

---
+++ ruby/loops2_practice_problems.rb
@@ -0,0 +1,93 @@
+# 1. Write a while loop to print the numbers 1 through 10.
+
+# nums = [*(1..10)]
+# i = 0
+# while i < nums.length
+#   p nums[i]
+#   i += 1
+# end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+
+# i = 0
+# while i < 5
+#   p "hello"
+#   i += 1
+# end
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+# while true
+#   puts "Enter a word: "
+#   word = gets.chomp
+#   if word == "stop"
+#     break
+#   end
+# end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+# i = 0
+# while i <= 100
+#   p i
+#   i += 5
+# end
+
+# 5. Write a while loop that prints the number 9000 ten times.
+
+# i = 0
+# while i < 10
+#   p 9000
+#   i += 1
+# end
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# while true
+#   p "enter a number: "
+#   num = gets.chomp.to_i
+#   if num > 10
+#     break
+#   end
+# end
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+
+# i = 50
+# while i <= 70
+#   p i
+#   i += 1
+# end
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+# i = 1
+# while i < 145
+#   p "Around the world"
+#   i += 1
+# end
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# while true
+#   p "enter a word: "
+#   word = gets.chomp
+#   if word.length > 5
+#     break
+#   end
+# end
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+
+# nums = [*(0..40)]
+# i = 2
+# while i <= nums[-1]
+#   p nums[i]
+#   i += 2
+# end
+
+
+# 1: 14 mins
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
